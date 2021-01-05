f8d950af40fbff982c7660f84e83fc37202750d1
darshanaswani <darshan.aswani64@gmail.com>
2021-01-05 13:50:13 +0530

    updating our application with reselect our application with reselect,replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,9 +3,11 @@ import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
+    {console.log("cartDropDown")}
     <div className="cart-items">
       {cartItems.map((cartItem) => (
         <CartItem key={cartItem.id} item={cartItem} />
@@ -15,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -7,6 +7,7 @@ const INITIAL_STATE = {
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
+  console.log(state);
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
@@ -15,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       };
     case CartActionTypes.ADD_ITEM:
       return {
-        ...state,
+        ...state,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
